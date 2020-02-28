ba96787e6f53ef9a7905809db66586f9892d0074
jeamick <nounahon.j@gmail.com>
2020-02-28 01:50:11 +0000

    Update clean test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -13,13 +13,15 @@ print(s)
 url = "http://test.com"
 payload_content = "example payload content"
 
-print("Setting payload: " + payload_content)
+print("Setting payload: {payload_content}")
 s.set_payload(url, payload_content)
+
 print("Getting payload: ")
 return_payload = s.get_payload(url)
 print(return_payload)
+
 print("Erasing payload")
 s.erase_payload(url)
+
 new_payload=s.get_payload(url)
-print("New payload:")
-print(new_payload)
+print("New payload: {new_payload}")
